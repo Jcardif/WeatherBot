b44b89cd0da5861846bd21cc705ff2b0eb64c247
Josh Cardif <josh.cardif14@outlook.com>
2018-01-27 16:17:05 +0300

    Initialised weather dialog

--- WeatherBot/Controllers/MessagesController.cs
+++ WeatherBot/Controllers/MessagesController.cs
@@ -18,7 +18,7 @@ namespace WeatherBot
         {
             if (activity.Type == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+                await Conversation.SendAsync(activity, () => new Dialogs.WeatherDialog());
             }
             else
             {
---
+++ WeatherBot/Dialogs/WeatherDialog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace WeatherBot.Dialogs
+{
+    [Serializable]
+    public class WeatherDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            
+        }
+    }
+}
