b18ac5d22a223d6701bc11904aff088c6cb9adef
Josh Cardif <josh.cardif14@outlook.com>
2018-02-10 10:25:28 +0300

    New azure bot service solution with luis template

--- WeatherBot/publish.cmd
+++ WeatherBot/publish.cmd
@@ -1,3 +1,3 @@
 nuget restore
-msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=mysimplewetherbot-Web-Deploy.pubxml -p:Password=znBxStyiqtbwnlsD9WeCtv4bj0B6ajGAmFCT0X65Rp546MxvescSW53RvS5x
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=weatherbotke-Web-Deploy.pubxml -p:Password=1qSms03bobv3WYxzyZPZg8xFxsfxeXhc7xF05fFnvCMH4xCPRmTfaAduhZ7D
 
