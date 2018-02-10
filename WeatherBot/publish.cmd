4d979d1cb71aa72580e89d22e0c4f91ebdae2c08
Josh Cardif <josh.cardif14@outlook.com>
2018-02-10 15:00:45 +0300

    Weather api

--- WeatherBot/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=weatherbotke-Web-Deploy.pubxml -p:Password=1qSms03bobv3WYxzyZPZg8xFxsfxeXhc7xF05fFnvCMH4xCPRmTfaAduhZ7D
-
