e4fb3800339b100f20855601b80a7e6be42db136
Josh Cardif <josh.cardif14@outlook.com>
2018-02-10 15:00:02 +0300

    Connected to the weather api

--- WeatherBotKe/Dialogs/BasicLuisDialog.cs
+++ WeatherBotKe/Dialogs/BasicLuisDialog.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Bot.Sample.LuisBot
 {
@@ -28,7 +32,19 @@ namespace Microsoft.Bot.Sample.LuisBot
         [LuisIntent("getWeather")]
         public async Task GetWeatherIntent(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("Getweather");
+            string weatherInfo;
+            foreach (var entity in result.Entities)
+            {
+                var locationInfo = entity.Entity;
+                var currentObservation = await GetCurrentWeatherUsingAPI(locationInfo);
+                string displayLocation = currentObservation.display_location?.full;
+                decimal tempC = currentObservation.temp_c;
+                string weather = currentObservation.weather;
+                weatherInfo = $"It is {weather} and {tempC} degrees in { displayLocation}.";
+                string icon = currentObservation.icon;
+
+                await context.PostAsync(weatherInfo);
+            }
             context.Wait(MessageReceived);
         }
    
@@ -51,6 +67,32 @@ namespace Microsoft.Bot.Sample.LuisBot
             
         }
 
-
+        private static async Task<dynamic> GetCurrentWeatherUsingAPI(string location)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var escapedLocation = Regex.Replace(location, @"\W+", "_");
+                    var jsonString = await
+                    client.GetStringAsync($"http://api.wunderground.com/api/0f4f72363a56f28b/conditions/q/{escapedLocation}.json");
+                    dynamic response = JObject.Parse(jsonString);
+                    dynamic observation = response.current_observation;
+                    dynamic results = response.response.results;
+                    if (observation != null)
+                    {
+                        return observation;
+                    }
+                    else if (results != null)
+                    {
+                        return null;
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+                return null;
+            }
+        }
     }
 }
